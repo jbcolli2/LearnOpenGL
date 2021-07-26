b99671d591d061034a90c6518a88800ab12ede62
Jeb Collins <jbcolli2@gmail.com>
2021-07-26 08:47:40 -0400

    Create basic window.

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -5,10 +5,57 @@
 //  Created by Jeb Collins on 7/25/21.
 //
 
+
+#define GL_SILENCE_DEPRECATION
 #include <iostream>
+//#include <glad/glad.h>
+#include <SFML/OpenGL.hpp>
+#include <SFML/Window.hpp>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    sf::ContextSettings settings;
+    settings.depthBits = 24;
+    settings.stencilBits = 8;
+    settings.antialiasingLevel = 0;
+    settings.attributeFlags = sf::ContextSettings::Attribute::Core;
+    settings.majorVersion = 3;
+    settings.minorVersion = 3;
+
+    sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, settings);
+    
+    
+    
+    
+    // run the main loop
+        bool running = true;
+        while (running)
+        {
+            // handle events
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                {
+                    // end the program
+                    running = false;
+                }
+                else if (event.type == sf::Event::Resized)
+                {
+                    // adjust the viewport when the window is resized
+                    glViewport(0, 0, event.size.width, event.size.height);
+                }
+            }
+
+            // clear the buffers
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            // draw...
+
+            // end the current frame (internally swaps the front and back buffers)
+            window.display();
+        }
+    
+    
     return 0;
 }
