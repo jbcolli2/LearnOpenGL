2297783ae8be7f7349deb23a4a19ee144c5f1669
Jeb Collins <jbcolli2@gmail.com>
2021-07-28 07:25:17 -0400

    Include VBO code

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -13,132 +13,120 @@
 
 
 
-//void processInput(sf::Window& window, bool& running)
-//{
-//    // handle events
-//    sf::Event event;
-//    while (window.pollEvent(event))
-//    {
-//        if (event.type == sf::Event::Closed)
-//        {
-//            // end the program
-//            running = false;
-//        }
-//        else if (event.type == sf::Event::Resized)
-//        {
-//            // adjust the viewport when the window is resized
-//            glViewport(0, 0, event.size.width, event.size.height);
-//        }
-//    }
-//
-//    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q))
-//    {
-//        running = false;
-//    }
-//
-//
-//}
-//
-//
-//
-//
-//void draw()
-//{
-//    float verts[] = {
-//        -.5f, -.5f, 0.0f,
-//        .5f,  -.5f, 0.0f,
-//        0.0f, .5f,  0.0f
-//    };
-//
-//
-//
-//
-//    // Vertex Shader
-//    const char* vertShaderSource = "#version 330 core\n"
-//    "layout (location = 0) in vec3 pos;\n"
-//    "void main()\n"
-//    "{\n"
-//    "gl_Position = vec4(pos, 0.0f);\n"
-//    "}\0";
-//
-//    unsigned int vertShader;
-//    vertShader = glCreateShader(GL_VERTEX_SHADER);
-//
-//    glShaderSource(vertShader, 1, &vertShaderSource, NULL);
-//    glCompileShader(vertShader);
-//
-//    // Check compile status
-//    int vertSuccess;
-//    char infoLog[512];
-//    glGetShaderiv(vertShader, GL_COMPILE_STATUS, &vertSuccess);
-//    if(!vertSuccess)
-//    {
-//        glGetShaderInfoLog(vertShader, 512, NULL, infoLog);
-//        std::cout << "VertexShader::Compile::Fail  " << infoLog << std::endl;
-//    }
-//
-//
-//
-//    // Fragment Shader
-//    const char* fragShaderSource = "#version 330 core\n"
-//    "out vec4 FragColor;\n"
-//    "void main()\n"
-//    "{\n"
-//        "FragColor = vec4(1.0f, 0.1f, 1.0f, 1.0f);\n"
-//    "}\0";
-//
-//    unsigned int fragShader;
-//    fragShader = glCreateShader(GL_FRAGMENT_SHADER);
-//
-//    glShaderSource(fragShader, 1, &fragShaderSource, NULL);
-//    glCompileShader(fragShader);
-//
-//    // Check compile status
-//    int fragSuccess;
-//    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &fragSuccess);
-//    if(!fragSuccess)
-//    {
-//        glGetShaderInfoLog(fragShader, 512, NULL, infoLog);
-//        std::cout << "FragmentShader::Compile::Fail  " << infoLog << std::endl;
-//    }
-//
-//
-//    unsigned int shaderProgram;
-//    shaderProgram = glCreateProgram();
-//    glAttachShader(shaderProgram, vertShader);
-//    glAttachShader(shaderProgram, fragShader);
-//    glLinkProgram(shaderProgram);
-//    int linkSuccess;
-//    glGetShaderiv(shaderProgram, GL_LINK_STATUS, &linkSuccess);
-//    if(!fragSuccess)
-//    {
-//        glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
-//        std::cout << "ShaderProgram::Linkerr::Fail  " << infoLog << std::endl;
-//    }
-//
-//
-//    // VBO
-//    unsigned int VBO;
-//    glGenBuffers(1, &VBO);
-//
-//    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-//    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
-//
-//
-//
-//
-//
-//    glUseProgram(shaderProgram);
-//    glDeleteShader(vertShader);
-//    glDeleteShader(fragShader);
-//
-//
-//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
-//    glEnableVertexAttribArray(0);
-//
-//
-//    glDrawArrays(GL_TRIANGLES, 0, 3);
-//}
+
+
+
+
+void draw()
+{
+    float verts[] = {
+        -.5f, -.5f, 0.0f,
+        .5f,  -.5f, 0.0f,
+        0.0f, .5f,  0.0f
+    };
+
+
+
+
+    // Vertex Shader
+    const char* vertShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 pos;\n"
+    "void main()\n"
+    "{\n"
+    "gl_Position = vec4(pos, 0.0f);\n"
+    "}\0";
+
+    unsigned int vertShader;
+    vertShader = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertShader, 1, &vertShaderSource, NULL);
+    glCompileShader(vertShader);
+
+    // Check compile status
+    int vertSuccess;
+    char infoLog[512];
+    glGetShaderiv(vertShader, GL_COMPILE_STATUS, &vertSuccess);
+    if(!vertSuccess)
+    {
+        glGetShaderInfoLog(vertShader, 512, NULL, infoLog);
+        std::cout << "VertexShader::Compile::Fail  " << infoLog << std::endl;
+    }
+
+
+
+    // Fragment Shader
+    const char* fragShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+        "FragColor = vec4(1.0f, 0.1f, 1.0f, 1.0f);\n"
+    "}\0";
+
+    unsigned int fragShader;
+    fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(fragShader, 1, &fragShaderSource, NULL);
+    glCompileShader(fragShader);
+
+    // Check compile status
+    int fragSuccess;
+    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &fragSuccess);
+    if(!fragSuccess)
+    {
+        glGetShaderInfoLog(fragShader, 512, NULL, infoLog);
+        std::cout << "FragmentShader::Compile::Fail  " << infoLog << std::endl;
+    }
+
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertShader);
+    glAttachShader(shaderProgram, fragShader);
+    glLinkProgram(shaderProgram);
+    int linkSuccess;
+    glGetShaderiv(shaderProgram, GL_LINK_STATUS, &linkSuccess);
+    if(!fragSuccess)
+    {
+        glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ShaderProgram::Linkerr::Fail  " << infoLog << std::endl;
+    }
+
+
+    // VBO
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+
+
+
+
+    glUseProgram(shaderProgram);
+    glDeleteShader(vertShader);
+    glDeleteShader(fragShader);
+
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+    
+    
+    
+    
+    
+    
+    glClearColor(0.0f,0.0f,0.0f,1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+
+
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -155,11 +143,6 @@ void processInput(GLFWwindow* window)
 }
 
 
-void draw()
-{
-    glClearColor(0.3f, 0.0f, 1.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-}
 
 
 
