a8108abcc700aa7660abd779b2e0845caff59e34
Jeb Collins <jbcolli2@gmail.com>
2021-07-28 10:11:10 -0400

    Update drawing code, still not working

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -7,6 +7,14 @@
 
 
 #define GL_SILENCE_DEPRECATION
+
+#ifdef __APPLE__
+#define glGenVertexArrays glGenVertexArraysAPPLE
+#define glBindVertexArray glBindVertexArrayAPPLE
+#define glDeleteVertexArrays glDeleteVertexArraysAPPLE
+#endif
+
+
 #include <iostream>
 //#include <glad/glad.h>
 #include <SFML/OpenGL.hpp>
@@ -41,6 +49,108 @@ void processInput(sf::Window& window, bool& running)
 }
 
 
+void initObjects(unsigned int& VAO, unsigned int& shaderProgram)
+{
+    float verts[] = {
+        -.5f, -.5f, 0.0f,
+        .5f,  -.5f, 0.0f,
+        0.0f, .5f,  0.0f
+    };
+
+
+
+
+    // Vertex Shader
+    const char* vertShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 pos;\n"
+    "void main()\n"
+    "{\n"
+    "gl_Position = vec4(pos.x,pos.y,pos.z, 1.0f);\n"
+    "}\0";
+
+    unsigned int vertShader;
+    vertShader = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertShader, 1, &vertShaderSource, NULL);
+    glCompileShader(vertShader);
+
+    // Check compile status
+    int vertSuccess;
+    char infoLog[512];
+    glGetShaderiv(vertShader, GL_COMPILE_STATUS, &vertSuccess);
+    if(!vertSuccess)
+    {
+        glGetShaderInfoLog(vertShader, 512, NULL, infoLog);
+        std::cout << "VertexShader::Compile::Fail  " << infoLog << std::endl;
+    }
+
+
+
+    // Fragment Shader
+    const char* fragShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+        "FragColor = vec4(1.0f, 0.1f, 1.0f, 1.0f);\n"
+    "}\0";
+
+    unsigned int fragShader;
+    fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(fragShader, 1, &fragShaderSource, NULL);
+    glCompileShader(fragShader);
+
+    // Check compile status
+    int fragSuccess;
+    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &fragSuccess);
+    if(!fragSuccess)
+    {
+        glGetShaderInfoLog(fragShader, 512, NULL, infoLog);
+        std::cout << "FragmentShader::Compile::Fail  " << infoLog << std::endl;
+    }
+
+
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertShader);
+    glAttachShader(shaderProgram, fragShader);
+    glLinkProgram(shaderProgram);
+    int linkSuccess;
+    glGetShaderiv(shaderProgram, GL_LINK_STATUS, &linkSuccess);
+    if(!fragSuccess)
+    {
+        glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ShaderProgram::Linkerr::Fail  " << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertShader);
+    glDeleteShader(fragShader);
+    
+
+    //******* VBO/VAO   ***************
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    //********* VAO **************
+
+}
+
+
+void draw()
+{
+    glClearColor(0.0f,0.0f,0.0f,1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
 
 
 
@@ -59,6 +169,10 @@ int main(int argc, const char * argv[]) {
     
     window.setActive(true);
     
+    unsigned int VAO;
+    unsigned int shaderProgram;
+    initObjects(VAO, shaderProgram);
+    
     
     // run the main loop
         bool running = true;
@@ -70,7 +184,10 @@ int main(int argc, const char * argv[]) {
             glClearColor(1.0f, 0.0f, 0.5f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
 
-            // draw...
+            glUseProgram(shaderProgram);
+            glBindVertexArray(VAO);
+            
+            draw();
 
             // end the current frame (internally swaps the front and back buffers)
             window.display();
