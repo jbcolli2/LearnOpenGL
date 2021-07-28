4460fd83972f5effba6d7af540fb1f3767684a2c
Jeb Collins <jbcolli2@gmail.com>
2021-07-28 07:18:03 -0400

    Hello Window with glfw

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -8,76 +8,209 @@
 
 #define GL_SILENCE_DEPRECATION
 #include <iostream>
-//#include <glad/glad.h>
-#include <SFML/OpenGL.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 
-void processInput(sf::Window& window, bool& running)
+
+//void processInput(sf::Window& window, bool& running)
+//{
+//    // handle events
+//    sf::Event event;
+//    while (window.pollEvent(event))
+//    {
+//        if (event.type == sf::Event::Closed)
+//        {
+//            // end the program
+//            running = false;
+//        }
+//        else if (event.type == sf::Event::Resized)
+//        {
+//            // adjust the viewport when the window is resized
+//            glViewport(0, 0, event.size.width, event.size.height);
+//        }
+//    }
+//
+//    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q))
+//    {
+//        running = false;
+//    }
+//
+//
+//}
+//
+//
+//
+//
+//void draw()
+//{
+//    float verts[] = {
+//        -.5f, -.5f, 0.0f,
+//        .5f,  -.5f, 0.0f,
+//        0.0f, .5f,  0.0f
+//    };
+//
+//
+//
+//
+//    // Vertex Shader
+//    const char* vertShaderSource = "#version 330 core\n"
+//    "layout (location = 0) in vec3 pos;\n"
+//    "void main()\n"
+//    "{\n"
+//    "gl_Position = vec4(pos, 0.0f);\n"
+//    "}\0";
+//
+//    unsigned int vertShader;
+//    vertShader = glCreateShader(GL_VERTEX_SHADER);
+//
+//    glShaderSource(vertShader, 1, &vertShaderSource, NULL);
+//    glCompileShader(vertShader);
+//
+//    // Check compile status
+//    int vertSuccess;
+//    char infoLog[512];
+//    glGetShaderiv(vertShader, GL_COMPILE_STATUS, &vertSuccess);
+//    if(!vertSuccess)
+//    {
+//        glGetShaderInfoLog(vertShader, 512, NULL, infoLog);
+//        std::cout << "VertexShader::Compile::Fail  " << infoLog << std::endl;
+//    }
+//
+//
+//
+//    // Fragment Shader
+//    const char* fragShaderSource = "#version 330 core\n"
+//    "out vec4 FragColor;\n"
+//    "void main()\n"
+//    "{\n"
+//        "FragColor = vec4(1.0f, 0.1f, 1.0f, 1.0f);\n"
+//    "}\0";
+//
+//    unsigned int fragShader;
+//    fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+//
+//    glShaderSource(fragShader, 1, &fragShaderSource, NULL);
+//    glCompileShader(fragShader);
+//
+//    // Check compile status
+//    int fragSuccess;
+//    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &fragSuccess);
+//    if(!fragSuccess)
+//    {
+//        glGetShaderInfoLog(fragShader, 512, NULL, infoLog);
+//        std::cout << "FragmentShader::Compile::Fail  " << infoLog << std::endl;
+//    }
+//
+//
+//    unsigned int shaderProgram;
+//    shaderProgram = glCreateProgram();
+//    glAttachShader(shaderProgram, vertShader);
+//    glAttachShader(shaderProgram, fragShader);
+//    glLinkProgram(shaderProgram);
+//    int linkSuccess;
+//    glGetShaderiv(shaderProgram, GL_LINK_STATUS, &linkSuccess);
+//    if(!fragSuccess)
+//    {
+//        glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
+//        std::cout << "ShaderProgram::Linkerr::Fail  " << infoLog << std::endl;
+//    }
+//
+//
+//    // VBO
+//    unsigned int VBO;
+//    glGenBuffers(1, &VBO);
+//
+//    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+//
+//
+//
+//
+//
+//    glUseProgram(shaderProgram);
+//    glDeleteShader(vertShader);
+//    glDeleteShader(fragShader);
+//
+//
+//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+//    glEnableVertexAttribArray(0);
+//
+//
+//    glDrawArrays(GL_TRIANGLES, 0, 3);
+//}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // handle events
-    sf::Event event;
-    while (window.pollEvent(event))
+    glViewport(0,0, width, height);
+};
+
+
+void processInput(GLFWwindow* window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE))
     {
-        if (event.type == sf::Event::Closed)
-        {
-            // end the program
-            running = false;
-        }
-        else if (event.type == sf::Event::Resized)
-        {
-            // adjust the viewport when the window is resized
-            glViewport(0, 0, event.size.width, event.size.height);
-        }
+        glfwSetWindowShouldClose(window, true);
     }
-    
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q))
-    {
-        running = false;
-    }
-    
-    
 }
 
 
-
+void draw()
+{
+    glClearColor(0.3f, 0.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
 
 
 
 int main(int argc, const char * argv[]) {
     
-    sf::ContextSettings settings;
-    settings.depthBits = 24;
-    settings.stencilBits = 8;
-    settings.antialiasingLevel = 0;
-    settings.attributeFlags = sf::ContextSettings::Attribute::Core;
-    settings.majorVersion = 3;
-    settings.minorVersion = 3;
-
-    sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, settings);
-    
-    window.setActive(true);
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     
     
-    // run the main loop
-        bool running = true;
-        while (running)
-        {
-            processInput(window, running);
-            
-            // clear the buffers
-            glClearColor(1.0f, 0.0f, 0.5f, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            // draw...
-
-            // end the current frame (internally swaps the front and back buffers)
-            window.display();
-        }
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if(window == NULL)
+    {
+        std::cout << "Failed to create glfw window\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
     
     
+    //GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    
+    glViewport(0,0,800, 600);
+    
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    
+    //Main loop
+    while(!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+        
+        
+        draw();
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    
+    glfwTerminate();
     return 0;
+    
 }
 
 
