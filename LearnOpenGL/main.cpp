23a4787a4df8f6e819ce0417041f5b44c701c106
Jeb Collins <jbcolli2@gmail.com>
2021-07-28 07:37:40 -0400

    Include triangle drawing code

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -91,15 +91,25 @@ void draw()
         std::cout << "ShaderProgram::Linkerr::Fail  " << infoLog << std::endl;
     }
 
+    
+    
 
-    // VBO
+    //******* VBO/VAO   ***************
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    
     unsigned int VBO;
     glGenBuffers(1, &VBO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
+    //********* VAO **************
+    
 
 
 
@@ -108,7 +118,7 @@ void draw()
     glDeleteShader(fragShader);
 
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    
     glEnableVertexAttribArray(0);
 
 
