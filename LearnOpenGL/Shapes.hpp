78dc7219eb77fd909dfded2740a4f20ff20c0940
Jeb Collins <jbcolli2@gmail.com>
2021-08-01 15:08:03 -0400

    Image and texture loading code in Shape

--- LearnOpenGL/Shapes.hpp
+++ LearnOpenGL/Shapes.hpp
@@ -14,8 +14,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 #include "OpenGLUtil.hpp"
-
 
 
 
@@ -26,25 +27,52 @@
 
 class Shape
 {
+    
+protected:
+    unsigned int VAO, texture;
+
+    
 public:
     Shape() {};
-    
     void virtual draw() = 0;
+  
+    
+    
+protected:
+    void loadTexture(const char* filename)
+    {
+        int width, height, nrChannels;
+        unsigned char* data = stbi_load(filename, &width, &height, &nrChannels, 0);
+        
+        if(data)
+        {
+            glGenTextures(1, &texture);
+            glBindTexture(GL_TEXTURE_2D, texture);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+            
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Failure to load texture " << filename << std::endl;
+        }
+    }
 };
+
+
+
+
 
 
 
 template <class T>
 class Triangle : public Shape
 {
-    unsigned int VAO;
-    std::vector<T> verts;
 
-    
+    std::vector<T> verts;
 public:
     Triangle(std::vector<T> vert);
-    
-//    Triangle(std::vector<VertColorData> verts);
     
     void virtual draw() override;
 };
@@ -54,7 +82,7 @@ public:
 template <class T>
 class Square : public Shape
 {
-    unsigned int VAO;
+    unsigned int VAO, texture;
     std::vector<T> verts;
     std::vector<unsigned int> indices = {0,1,2, 0, 2,3};
 
